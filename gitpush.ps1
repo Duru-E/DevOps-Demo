ab69a5754e046f0c3d03c365ba830737dd074b24
JaY-III <jordanstella@yahoo.com>
2026-04-09 21:31:18 -0400

    Added with push script

--- gitpush.ps1
+++ gitpush.ps1
@@ -1,22 +1,25 @@
-cls
-write-host "This will update to current build" -ForegroundColor DarkMagenta
- 
-git clone https://github.com/Duru-E/DevOps-Demo.git
-git pull
+Write-Host "This will update to current build" -ForegroundColor DarkMagenta
 
+$repo = "https://github.com/Duru-E/DevOps-Demo.git"
+# if current folder is a git repo with that remote, pull; otherwise fetch into temp and copy
+$remoteUrl = git config --get remote.origin.url 2>$null
+if ($LASTEXITCODE -eq 0 -and $remoteUrl -eq $repo) {
+    git pull origin main
+} else {
+    $tmp = Join-Path $env:TEMP ("tmprepo_{0}" -f ([guid]::NewGuid().ToString()))
+    git clone $repo $tmp
+    robocopy $tmp . /MIR /XD .git
+    Remove-Item -Recurse -Force $tmp
+}
 
-write-host "deal with the errors, im not writting if statements, this works" -ForegroundColor Magenta
-Write-host "Make a branch for the demo"  -ForegroundColor DarkMagenta
+Write-Host "Make a branch for the demo" -ForegroundColor DarkMagenta
+$branch = 'The_Prestige'
+if (git rev-parse --verify $branch 2>$null) { git checkout $branch } else { git checkout -b $branch }
 
-git checkout The_Prestige
-git checkout -b The_Prestige
-
-
-write-host "Adding the change, stage and commit" -ForegroundColor DarkMagenta
-write-host "Ctrl + C to Cancel" -ForegroundColor DarkRed
-pause 
+Write-Host "Adding the change, stage and commit" -ForegroundColor DarkMagenta
+Write-Host "Ctrl+C to cancel" -ForegroundColor DarkRed
+Pause
 
 git add .
 git commit -m "Added with push script"
-git push origin The_Prestige
-
+git push -u origin $br
